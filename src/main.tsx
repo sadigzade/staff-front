17256c5d4f0facb408bf328e779d90db0fab9ff9
sadigzade <sadigzade.hi@mail.ru>
2024-02-26 23:42:35 +0300

    Added basic application routing + added fonts

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,32 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import { Provider } from "react-redux";
+import ReactDOM from "react-dom/client";
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+import { store } from "./app/store";
+
+import { Paths } from "./paths";
+import Login from "./pages/Login";
+import Register from "./pages/Register";
+
+import "./index.css";
+
+const router = createBrowserRouter([
+  {
+    path: Paths.home,
+    element: <h1>Home</h1>,
+  },
+  {
+    path: Paths.login,
+    element: <Login />,
+  },
+  {
+    path: Paths.register,
+    element: <Register />,
+  },
+]);
+
+ReactDOM.createRoot(document.getElementById("root")!).render(
+  <Provider store={store}>
+    <RouterProvider router={router} />
+  </Provider>,
+);
---
+++ src/paths.ts
@@ -0,0 +1,9 @@
+export const Paths = {
+  home: "/",
+  employee: "/employee",
+  employeeAdd: "/employee/add",
+  employeeEdit: "/employee/edit",
+  status: "/status",
+  login: "/login",
+  register: "/register",
+} as const;
